5328ebb4cf42da25e1dd5dedb2a006443343d9c4
Dishant Dilip Yashwante <113447476+dishant1996@users.noreply.github.com>
2022-11-12 22:37:33 +0530

    Implementing StackNQueue UC14 Assignment

---
+++ UC-4/UC4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StackandQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Stack stack = new Stack();
+            // stack.push(56);
+            // stack.push(30);
+            // stack.push(70);
+            // stack.display();
+            // stack.peak();
+            // stack.pop();
+            // stack.peak();
+            // stack.pop();
+            // stack.peak();
+            // stack.pop();
+            // stack.display();
+
+
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.display();
+            queue.Dequeue();
+            queue.display();
+            queue.Dequeue();
+            queue.display();
+            queue.Dequeue();
+            queue.display(); ;
+        }
+    }
+}
